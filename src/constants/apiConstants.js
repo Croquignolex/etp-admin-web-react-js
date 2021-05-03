ce629154e4c297c32f2c999d7cfc0f1133328082
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-05-03 10:09:53 +0100

    Improve saga

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -28,13 +28,6 @@ export const NEW_CLEARANCE_API_PATH = `${API_URL}/demande_destockage_agent`;
 export const ALL_CLEARANCES_API_PATH = `${API_URL}/list_demandes_destockage_all`;
 export const DECLARE_CLEARANCE_API_PATH = `${API_URL}/reponse_demandes_destockage`;
 
-// Sims
-export const SIM_API_PATH = `${API_URL}/show_puce`;
-export const SIMS_API_PATH = `${API_URL}/puce_list`;
-export const EDIT_SIM_API_PATH = `${API_URL}/edit_puce`;
-export const CREATE_SIM_API_PATH = `${API_URL}/store_puce`;
-export const All_SIMS_API_PATH = `${API_URL}/puce_list_all`;
-
 // Agents
 export const AGENT_API_PATH = `${API_URL}/show_agent`;
 export const AGENTS_API_PATH = `${API_URL}/list_agents`;
@@ -133,3 +126,13 @@ export const REFUELS_API_PATH = `${API_URL}/list_destockage_collector`;
 
 // Affords
 export const AFFORDS_API_PATH = `${API_URL}/list_approvisionnement_collector`;
+
+// Sims
+export const SIM_API_PATH = `${API_URL}/show_puce`;
+export const SIMS_API_PATH = `${API_URL}/puce_list`;
+export const EDIT_SIM_API_PATH = `${API_URL}/edit_puce`;
+export const CREATE_SIM_API_PATH = `${API_URL}/store_puce`;
+export const AGENTS_SIMS_API_PATH = `${API_URL}/puce_list_all_agent`;
+export const FLEETS_SIMS_API_PATH = `${API_URL}/puce_list_gestionnaire`;
+export const COLLECTORS_SIMS_API_PATH = `${API_URL}/puce_list_collector`;
+export const RESOURCES_SIMS_API_PATH = `${API_URL}/puce_list_all_resource`;
